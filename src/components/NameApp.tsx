a6b14bc74cee7f6a60e13cac5c2f91d9981ce9e9
Anju1023 <niafuguapricottaro@gmail.com>
2025-08-17 23:24:10 +0900

    スタイルを改善し、NameAppコンポーネントの見た目を向上させました。タイトルのフォントサイズを変更し、入力フィールドにスタイルを追加しました。

--- src/components/NameApp.tsx
+++ src/components/NameApp.tsx
@@ -7,16 +7,17 @@ export default function NameApp() {
 
 	return (
 		<div className="p-8">
-			<h1>お名前教えて🫶</h1>
+			<h1 className="text-4xl mb-8">お名前教えて🫶</h1>
 
 			<input
 				type="text"
 				value={name}
 				placeholder="お名前を入力してねえ"
 				onChange={(e) => setName(e.target.value)}
+				className="border p-3 rounded mb-4 w-full"
 			/>
 
-			<div>
+			<div className="text-2xl">
 				{name
 					? `こんにちは！${name}ちゃん！！`
 					: 'まだ入力されてないよおおおお；；'}
